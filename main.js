c948481fb776cdb92a7884084dbe3b44e705696e
oliverdd <43624923+QAQddbest@users.noreply.github.com>
2019-12-29 18:56:02 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let suckIt = `<div style="position: fixed;top: 50%;z-index: 999;right: 10%;padding: 1em;background: rgba(233,233,233,.6);border: 1px solid #aaa;">
-<textarea placeholder="评价内容" id="fuckMe" style="padding: 10px;height: 60px;width: 200px;margin: 10px;">Good Job Bro.</textarea>
+<textarea placeholder="评价内容" id="fuckMe" style="padding: 10px;height: 60px;width: 200px;margin: 10px;">拉满拉满！！！</textarea>
 <p style="text-align: center;">
 <input type="text"  id="times" placeholder="份数，1 ~ 20" style="width: 100px;padding: .4em;margin: 0 1em;background: #fff;border: 1px solid #999;float: left;" value="1">
 <input type="submit" value="Fuck It Now" onclick="fuckIt();" style="border: 1px solid #aaa;padding: 5px;color: #999;cursor: pointer;">
